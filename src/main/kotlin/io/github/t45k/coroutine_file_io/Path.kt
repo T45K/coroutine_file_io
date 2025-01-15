25984557146a19fdc342a0ad0d6782a6a1cddae4
T45K <tasktas9@gmail.com>
2025-01-16 00:24:41 +0900

    add file open option

--- src/main/kotlin/io/github/t45k/coroutine_file_io/Path.kt
+++ src/main/kotlin/io/github/t45k/coroutine_file_io/Path.kt
@@ -7,14 +7,15 @@ import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousFileChannel
 import java.nio.charset.Charset
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 
 suspend fun Path.readTextAsync(charset: Charset = Charsets.UTF_8): String {
     val byteBuffer = ByteBuffer.allocate(fileSize().toInt())
-    AsynchronousFileChannel.open(this).use { it.aRead(byteBuffer, 0) }
+    AsynchronousFileChannel.open(this, StandardOpenOption.READ).use { it.aRead(byteBuffer, 0) }
     return byteBuffer.flip().getByteString().decodeToString(charset)
 }
 
 suspend fun Path.writeTextAsync(text: String, charset: Charset = Charsets.UTF_8) {
-    val byteBuffer = ByteBuffer.wrap(text.toByteArray(charset)).flip()
-    AsynchronousFileChannel.open(this).use { it.aWrite(byteBuffer, 0) }
+    val byteBuffer = ByteBuffer.wrap(text.toByteArray(charset))
+    AsynchronousFileChannel.open(this, StandardOpenOption.WRITE).use { it.aWrite(byteBuffer, 0) }
 }
